e8db0b385b4908ce2a61d1d9581ca9f4c45ff8b1
Mohamed Hassan <mohamed.hassan.mohy.01@gmail.com>
2026-09-06 13:29:03 +0300

    Initial Commit

---
+++ Lesson/1.10/Data_Modiling.sql
@@ -0,0 +1,20 @@
+--Information schema
+SELECT
+    *
+From information_schema.tables
+WHERE table_catalog = 'data_jobs';
+
+SELECT
+    *
+From information_schema.columns
+WHERE table_catalog = 'data_jobs';
+
+SELECT
+    *
+From information_schema.table_constraints
+WHERE table_catalog = 'data_jobs';
+
+PRAGMA show_tables;
+
+Describe job_postings_fact;
+
---
+++ Lesson/1.9/1.9_VScode_Intro.sql
@@ -0,0 +1,1 @@
+duckdb md:data_jobs
